6e0f49401545ba095b7d3dc7e8ac9def7d5e7191
Evan Lezar <mail@evanlezar.com>
2014-06-14 15:27:31 +0200

    Add a basic test for a beautifier that makes no changes.

---
+++ fbeautifier/beautifier.py
@@ -0,0 +1,11 @@
+
+
+__all__ = ['beautify']
+
+
+def beautify(input_string, options=None):
+  """
+  Beautify an input string.
+  """
+  if options is None:
+    return input_string
--- fbeautifier/tests/test_module_import.py
+++ fbeautifier/tests/test_module_import.py
@@ -4,3 +4,27 @@ Test the basic importing of the module.
 
 def test_module_import():
   import fbeautifier
+
+
+def test_no_change():
+  from fbeautifier.beautifier import beautify
+  source_string = """
+  MODULE modname
+  IMPLICIT NONE
+
+  CONTAINS
+
+  SUBROUTINE SUB1()
+
+  END SUBROUTINE SUB1
+
+  END MODULE modname
+  """
+
+
+  beautified_string = beautify (source_string, options=None)
+
+  assert (source_string == beautified_string)
+
+
+
