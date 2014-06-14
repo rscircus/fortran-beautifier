c0bb6d253a71375d7d39077b8209c9db7a5601c2
Evan Lezar <mail@evanlezar.com>
2014-06-14 15:53:58 +0200

    Add a simple wrapper for fparser.

--- fbeautifier/beautifier.py
+++ fbeautifier/beautifier.py
@@ -3,9 +3,28 @@
 __all__ = ['beautify']
 
 
+import re
+
+def beautify_using_fparser(input_string):
+  from fparser.readfortran import FortranStringReader
+  from fparser.parsefortran import  FortranParser
+  reader = FortranStringReader(input_string)
+
+  parser = FortranParser(reader, False)
+  parser.parse()
+  parser.analyze()
+
+  parser_output = parser.block.tofortran()
+
+  return re.sub(r'^.*?BEGINSOURCE.*free\n', r'', parser_output, re.M+re.S)
+
+
+
 def beautify(input_string, options=None):
   """
   Beautify an input string.
   """
   if options is None:
     return input_string
+  elif options == 'fparser':
+    return beautify_using_fparser(input_string)
--- fbeautifier/tests/test_module_import.py
+++ fbeautifier/tests/test_module_import.py
@@ -2,13 +2,12 @@
 Test the basic importing of the module.
 """
 
+from nose.tools import *
+
 def test_module_import():
   import fbeautifier
 
-
-def test_no_change():
-  from fbeautifier.beautifier import beautify
-  source_string = """
+source_string = """
   MODULE modname
   IMPLICIT NONE
 
@@ -21,10 +20,32 @@ def test_no_change():
   END MODULE modname
   """
 
+def test_no_change():
+  from fbeautifier.beautifier import beautify
 
   beautified_string = beautify (source_string, options=None)
 
-  assert (source_string == beautified_string)
+  eq_(source_string, beautified_string)
+
+
+def test_use_parser():
+  from fbeautifier.beautifier import beautify
+
+  expected_string = """
+  MODULE modname
+    IMPLICIT NONE
+
+    CONTAINS
+
+    SUBROUTINE sub1()
+
+    END SUBROUTINE sub1
+
+  END MODULE modname
+"""
+
+  beautified_string = beautify (source_string, options='fparser')
+  eq_(expected_string, beautified_string)
 
 
 
